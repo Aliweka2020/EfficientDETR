351cb710e0adb3f712d3c0ce1add4b9314a38eb4
Aliweka2020 <aweka220@YAHOO.COM>
2022-02-09 10:36:06 -0800

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import torch.utils.data
+import torchvision
+
+from .coco import build as build_coco
+
+
+def get_coco_api_from_dataset(dataset):
+    for _ in range(10):
+        # if isinstance(dataset, torchvision.datasets.CocoDetection):
+        #     break
+        if isinstance(dataset, torch.utils.data.Subset):
+            dataset = dataset.dataset
+    if isinstance(dataset, torchvision.datasets.CocoDetection):
+        return dataset.coco
+
+
+def build_dataset(image_set):
+    dataset_file = 'coco'
+    if dataset_file == 'coco':
+        return build_coco(image_set)
+    raise ValueError(f'dataset {args.dataset_file} not supported')
